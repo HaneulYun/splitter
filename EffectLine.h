60928cb557029c61c33b10c092b95fd980ad0faa
jjangsu <a01056386069@gmail.com>
2018-10-09 20:32:12 +0900

    change EffectLine and RedLine class

--- EffectLine.h
+++ EffectLine.h
@@ -3,9 +3,14 @@
 
 class CEffectLine : public CVFXBase
 {
+	POINT* m_TargetPolygon;
 
+	float m_Distance;
+	float m_InnerRatio;
+	float m_OuterRatio;
+	CTimer m_Timer;
 public:
-	CEffectLine(Vector pt, COLORREF color, float distance, float direction);
+	CEffectLine(Vector pt, int polyAngle, float distance, COLORREF color);
 	~CEffectLine();
 
 	virtual bool Initialize() override;
